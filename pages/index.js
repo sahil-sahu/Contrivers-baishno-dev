f92a9e4275190acbcfa7ec6b6846a447fd16e2d7
sahil-sahu <sahilku2003@gmail.com>
2022-06-10 06:29:25 +0530

    firstCommit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import { NextSeo } from 'next-seo';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <NextSeo 
+        title="Baishnodev Builders"
+        description="Real Estates Company adomed with strong fundamentals on providing real quality homes to people that conform to their taste and style for an affordable price in and a rounding Bhubaneswar since last 11 years. "
+      />
+      <main>
+        
+      </main>  
+    </>
+  )
+}
