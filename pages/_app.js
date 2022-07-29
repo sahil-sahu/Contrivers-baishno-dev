1b3ef8be2f9c03f07fd0a6326ed2624ec2124005
sahil-sahu <sahilku2003@gmail.com>
2022-07-29 11:09:42 +0530

    added loading spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import Head from "next/head";
 
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import '../components/nprogress.css'; //styles of nprogress//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
